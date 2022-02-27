61c925f890c0e328451a9419f1556524f9d119de
Mark Kirby <markkirby1997@gmail.com>
2022-02-27 09:49:26 -0500

    added lines and sides toggles / text to home... as well as description

--- src/container/Home/home.jsx
+++ src/container/Home/home.jsx
@@ -7,9 +7,16 @@ const home = () => {
     <div className="container">
       <Header/>
       <h1>Make Your<br/>Ideas Come<br/>To Life</h1>
-      <div className="home__work__popout"></div>
-      <span className='home__topLeft__line'></span>
-      <span className='home__bottomLeft__line'></span>
+      <p className='home__description'>I’m an Independent Front-End Developer With<br/>an eye for creativity and professionalism</p>
+      <div className="home__work__popout">
+        <div><span>0</span><span>0</span><span>0</span><span>0</span></div>
+        <p>MY<br/>WORK</p>
+      </div>
+      <div className='home__topLeft__line'></div>
+      <div className='year__est'>2021<br/><span>Since</span></div>
+      <div className='home__bottomLeft__line'></div>
+      <div className="scroll__txt">SCROLL</div>
+      <div className="home__right__line"></div>
     </div>
   )
 }
