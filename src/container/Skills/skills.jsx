1568c9bc5f19dfefdb15e1cf5c8e96424d864137
Mark Kirby <markkirby1997@gmail.com>
2022-03-09 16:09:40 -0500

    added carousel however the sanity does not poulate

--- src/container/Skills/skills.jsx
+++ src/container/Skills/skills.jsx
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
+import {urlFor, client} from '../../client';
 import './skills.scss';
 
 
-const skills = () => {
+const Skills = () => {
+  
+
+  // Below will populate the dat from the Sanity Server 
+  const [skills, setSkills] = useState([]) // useStateSnippet
+  useEffect(() => { // useEffectSnippet
+    const query = `*[_type == "skills"]{title}`;
+
+    client.fetch(query)
+      .then((data) => setSkills(data))
+      .catch(console.error);
+
+  }, [])
+  
+
   return (
     <div className="skills__container">
       <div className="left">
@@ -15,9 +30,26 @@ const skills = () => {
           <p>The Languages, Skills, And Techniques<br/>I Use To Bring Your Ideas To Life.</p>
         </div>
       </div>
-      <div className="right"></div>
+      <div className="right">
+        <div className="carousel__container">
+          {/* 
+            const skills = [
+              {title: 'HTML'},
+            ];
+          */}
+          {skills.map((skill,index) => (
+              <div key={`item-${index}`} className="skill">
+                <div className='circle'>
+                  <div className="icon__circle"></div>
+                  {skill.title}
+                </div>
+              </div>
+          ))}
+
+        </div>
+      </div>
     </div>
   )
 }
 
-export default skills
+export default Skills
