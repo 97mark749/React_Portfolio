37ced3f345d11d69bce31446faddf13aca68af9f
Mark Kirby <markkirby1997@gmail.com>
2022-03-09 10:32:27 -0500

    positioned the phone element and added the services header and listed items

--- src/container/Services/services.jsx
+++ src/container/Services/services.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import './services.scss';
 import {Phone} from '../../components';
+import {BsFillCheckCircleFill} from 'react-icons/bs';
 
 
 const services = () => {
+  const services = ['SEO Optimization','Expectional UX/UI Design','Client Tailored Website Design','Interactive User Interfaces','A Simple Yet Modern Approach','Mobile First Responsiveness','User Engagement'];
   return (
     <div className="services__container">
-        <div className="left"></div>
-        <div className="right"><Phone/></div>
+        <div className="left">
+          <h2 className='header'>Services &<br/>Expectations</h2>
+          <ul className='services__list'>
+            {services.map((item) => (
+              <div key={`item-${item}`} className="list__item"><BsFillCheckCircleFill className='check_mark'/><li>{item}</li></div>
+            ))}
+          </ul>
+        </div>
+        <div className="right"><div className="phone__img"><Phone/></div></div>
     </div>
   );
 }
