d2329e48b4b7a445c4478fc95422ffecc5041863
Mark Kirby <markkirby1997@gmail.com>
2022-03-11 16:06:00 -0500

    finished contact form

--- src/container/Contact/contact.jsx
+++ src/container/Contact/contact.jsx
@@ -6,26 +6,38 @@ const contact = () => {
   return (
    <div className="contact__container">
       <form action="" method="post" className="contact__form__container">
+        <div className="contact__banner">
+          <h4 className="sub__header">Send Me A Message</h4>
+          <p>Have A Question Or Proposal, Don’t Hesitate To Contact Me.</p>
+        </div>
         <div className="left">
-          <div className="input__container">
-            <input type="text" name="fName" id="firstName" placeholder="First Name" required/>
-            <span></span>
+          <div className='input__boxes'>
+            <div className="name">
+              <div className="input__container">
+                <input type="text" name="fName" id="firstName" placeholder="First Name" tabIndex={0} required/>
+                <span></span>
+              </div>
+              <div className="input__container">
+                <input type="text" name="lName" id="lastName" placeholder="Last Name" tabIndex={1} required/>
+                <span></span>
+              </div>
+            </div>
+            <div className="input__container lg">
+              <input type="email" name="email" id="email" placeholder="Email" tabIndex={2} required/>
+              <span></span>
+            </div>
+            <div className="input__container lg">
+              <input type="tel" name="pNumber" id="phoneNumber" placeholder="Phone Number (Optional)" tabIndex={3}/>
+              <span></span>
+            </div>
           </div>
-          <div className="input__container">
-            <input type="text" name="lName" id="lastName" placeholder="Last Name" required/>
-            <span></span>
-          </div>
-          <div className="input__container lg">
-            <input type="email" name="email" id="email" placeholder="Email" required/>
-            <span></span>
-          </div>
-          <div className="input__container lg">
-            <input type="number" name="pNumber" id="phoneNumber" placeholder="Phone Number (Optional)"/>
-            <span></span>
+          <div className="form__buttons">
+            <input type="button" value="RESET" className='btn'/>
+            <input type="button" value="SUBMIT" className='btn' tabIndex={4}/>
           </div>
         </div>
         <div className="right">
-          <textarea name="comments" id="comments" cols="30" rows="10" required></textarea>
+          <textarea name="comments" id="comments" cols="30" rows="10" placeholder= "Write A Brief Message..." required/>
         </div>
       </form>
     </div>
