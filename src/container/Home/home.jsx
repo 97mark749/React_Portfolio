56f3098f0a53dd0b0e4fc37c7078e6890107d60d
Mark Kirby <markkirby1997@gmail.com>
2022-03-21 15:43:28 -0400

    added home square functionality

--- src/container/Home/home.jsx
+++ src/container/Home/home.jsx
@@ -1,20 +1,27 @@
 import React from 'react'
 import './home.scss'
+import '../../scripts';
 import {Header} from '../../components';
 import {images} from '../../constants';
 import {IoIosArrowDropdownCircle} from 'react-icons/io';
+import {MdStarBorderPurple500} from 'react-icons/md';
+import {IoChatbubbleEllipsesOutline} from 'react-icons/io5';
 
 const home = () => {
+  
+  
+
+
   return (
     <div className="container">
       <Header/>
       <h1>Make Your<br/>Ideas Come<br/>To Life</h1>
       <p className='home__description'>I’m an Independent Front-End Developer with<br/>an eye for creativity and professionalism</p>
       <div className="home__work__popout">
-        <div><span>0</span><span>0</span><span>0</span><span>0</span></div>
-        <p>MY<br/>WORK</p>
+        <div><span><MdStarBorderPurple500 className='popout__icon'/></span><span><IoChatbubbleEllipsesOutline className='popout__icon'/></span><span>0</span><span>0</span></div>
+        <p>MY WORK</p>
       </div>
-      <div className='home__topLeft__line'></div>
+      <div className='home__topLeft__line'><div className="line one"></div></div>
       <div className='year__est'>2021<br/><span>Since</span></div>
       <div className="image__ctr">
         <img src={images.blur_keyboard} alt="blurred keyboard"/>
@@ -24,9 +31,9 @@ const home = () => {
           <IoIosArrowDropdownCircle className='byb__icon'/>
         </div>
         </div>
-      <div className='home__bottomLeft__line'></div>
+      <div className='home__bottomLeft__line'><div className="line two"></div></div>
       <div className="scroll__txt">SCROLL</div>
-      <div className="home__right__line"></div>
+      <div className="home__right__line"><div className="line three"></div></div>
     </div>
   )
 }
